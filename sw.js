9dabf57577c25d04b81aca61325327ca424cddb9
Wendy <wuendy14@gmail.com>
2026-04-12 16:59:09 +0200

    primer commit

---
+++ sw.js
@@ -0,0 +1,17 @@
+const CACHE = 'calorias-v1';
+const FILES = [
+  '/calorias.html',
+  '/manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(FILES))
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
